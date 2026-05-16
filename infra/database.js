e6e141c06388c17582f70388e5dc11aab2f02c2f
EDERSON LOPES GOULART <goularteletricista@gmail.com>
2026-05-16 21:10:15 +0000

    remove a função getSSLValues e simplifica a configuração SSL do cliente PostgreSQL

--- infra/database.js
+++ infra/database.js
@@ -7,45 +7,17 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
-    ssl: getSSLValues(),
+    ssl: {
+      rejectUnauthorized: false,
+    },
   });
-  console.log(getSSLValues());
 
   try {
     await client.connect();
-    const result = await client.query(queryObject);
-    return result;
-  } catch (error) {
-    console.error("Database query error:", error);
-    throw error;
+    return await client.query(queryObject);
   } finally {
     await client.end();
   }
 }
 
-export default {
-  query: query,
-};
-
-function getSSLValues() {
-  if (process.env.POSTGRES_CA) {
-    const cert = process.env.POSTGRES_CA.replace(/\\n/g, "\n") // converte \n literais
-      .trim();
-
-    console.log("SSL preview:");
-    console.log(cert.substring(0, 60));
-
-    return {
-      rejectUnauthorized: true,
-      ca: cert,
-    };
-  }
-
-  return process.env.NODE_ENV === "development"
-    ? false
-    : { rejectUnauthorized: false };
-}
-
-console.log("RAW:");
-console.log(JSON.stringify(process.env.POSTGRES_CA));
-console.log(process.env.POSTGRES_CA?.substring(0, 100));
+export default { query };

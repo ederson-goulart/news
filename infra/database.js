d7b649617e39192f16940be480cbdc9e7e4ed996
EDERSON LOPES GOULART <goularteletricista@gmail.com>
2026-05-16 15:41:42 +0000

    atualiza configuração do cliente PostgreSQL para usar SSL com base nas variáveis de ambiente

--- infra/database.js
+++ infra/database.js
@@ -7,15 +7,9 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
-    ssl: true,
+    ssl: getSSLValues(),
   });
-  console.log("Credenciais Postgres", {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
-  });
+
   try {
     await client.connect();
     const result = await client.query(queryObject);
@@ -29,5 +23,14 @@ async function query(queryObject) {
 }
 
 export default {
-  query,
+  query: query,
 };
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+  return process.env.NODE_ENV === "development" ? false : true;
+}
